a25f7aa0556e954f865c378ff2d410748a2486c2
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2025-12-04 14:52:24 -0600

    got lazy and que'd AI on Registration with unfamiliar components and regret it. Part1 of working through it to understand.

---
+++ todoFrontend/src/components/Registration.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    import {ref, computed } from 'vue';
+    import { useRouter } from 'vue-router';
+
+    
+
+    const simpleValidation = computed(()=>{
+        return 
+    });
+</script>
+
+<template>
+    <form @submit.prevent>
+
+    </form>
+</template>
