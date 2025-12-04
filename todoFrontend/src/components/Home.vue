004c5c17e000f06c4e1f08cdb44e56246801f8f9
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2025-12-04 16:14:31 -0600

    really rough login, untested and untinkered.

---
+++ todoFrontend/src/components/Home.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import Login from './Login.vue';
+</script>
+
+<template>
+    <h1>Home</h1>
+    <Login></Login>
+</template>
+
+<style scoped>
+
+</style>
--- todoFrontend/src/components/Login.vue
+++ todoFrontend/src/components/Login.vue
@@ -1,8 +1,15 @@
 <template>
     <form>
-
-        <input id="username"></input>
-        <input id="password"></input>
+        <p>Login</p>
+        <label for="username">Username:</label>
+        <input id="username" type="text" required></input>
+        <label for="password">Password:</label>
+        <input id="password" type="password" required></input>
+        <label for="register">Don't have an account? Register now</label>
+        <input id="register" type="button"></input>
         <button>Submit</button>
     </form>
 </template>
+
+<style scoped>
+</style>
