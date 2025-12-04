9097126df5fe5811319687975d19a6d1760e506b
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2025-12-04 17:14:23 -0600

    base init login form styling.

--- todoFrontend/src/components/Login.vue
+++ todoFrontend/src/components/Login.vue
@@ -4,16 +4,30 @@ import RegisterNow from './RegisterNow.vue';
 </script>
 
 <template>
-    <h2>Login</h2>
-    <form>
-        <label for="username">Username:</label>
-        <input id="username" type="text" required></input>
-        <label for="password">Password:</label>
-        <input id="password" type="password" required></input>
-        <button>Submit</button>
-    </form>
-    <RegisterNow></RegisterNow>
+    <div>
+        <h2>Login</h2>
+        <form>
+            <label for="username">Username:</label>
+            <input id="username" type="text" required></input>
+            <label for="password">Password:</label>
+            <input id="password" type="password" required></input>
+            <button>Submit</button>
+        </form>
+        <RegisterNow></RegisterNow>
+    </div>
 </template>
 
 <style scoped>
+    div, form {
+        width: fit-content;
+        margin: 0 auto;
+    }
+    h2{
+        text-align: center;
+    }
+    form {
+        margin: 16px auto;
+        display: flex;
+        flex-direction: column;
+    }
 </style>
