031358bcac9d8e159e56447309d999ba5aa53c28
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2025-12-04 17:01:31 -0600

    may as well try to have a separate register now button? Might be too much compartmentalization, oh well.

--- todoFrontend/src/components/Login.vue
+++ todoFrontend/src/components/Login.vue
@@ -1,14 +1,18 @@
+<script setup lang="ts">
+import RegisterNow from './RegisterNow.vue';
+
+</script>
+
 <template>
+    <h2>Login</h2>
     <form>
-        <p>Login</p>
         <label for="username">Username:</label>
         <input id="username" type="text" required></input>
         <label for="password">Password:</label>
         <input id="password" type="password" required></input>
-        <label for="register">Don't have an account? Register now</label>
-        <input id="register" type="button"></input>
         <button>Submit</button>
     </form>
+    <RegisterNow></RegisterNow>
 </template>
 
 <style scoped>
---
+++ todoFrontend/src/components/RegisterNow.vue
@@ -0,0 +1,4 @@
+<template>
+    <label for="register">Don't have an account?</label>
+    <input id="register" type="button" value="Register Now"></input>
+</template>
