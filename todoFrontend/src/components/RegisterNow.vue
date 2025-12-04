0a1f737f7fbc45988e6708986c4c937405168222
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2025-12-04 17:09:40 -0600

    init style register.

--- todoFrontend/src/components/RegisterNow.vue
+++ todoFrontend/src/components/RegisterNow.vue
@@ -1,4 +1,15 @@
 <template>
-    <label for="register">Don't have an account?</label>
-    <input id="register" type="button" value="Register Now"></input>
+    <div>
+        <label for="register">Don't have an account?</label>
+        <input id="register" type="button" value="Register Now"></input>
+    </div>
 </template>
+
+<style scoped>
+    div {
+        width: fit-content;
+        margin: 8px auto;
+        display: flex;
+        flex-direction: column;
+    }
+</style>
