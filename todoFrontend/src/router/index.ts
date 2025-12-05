40212af76437b8b349c8a66e2076a21a16b66829
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2025-12-04 19:00:52 -0600

    base AI route file.

--- todoFrontend/src/router/index.ts
+++ todoFrontend/src/router/index.ts
@@ -1,1 +1,19 @@
 import { RouteRecordRaw } from 'vue-router';
+import Home from '../components/Home.vue';
+import Registration from '../components/Registration.vue';
+
+const routes: RouteRecordRaw[] = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/register',
+        name: 'Registration',
+        component: Registration
+    }
+    //TODO -- additional routes
+];
+
+export default routes;
