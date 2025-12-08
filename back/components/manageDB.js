4febf1065915b9214da49616dda1bea98039ef1f
AJDev <contact@andrewjonhardt.com>
2025-12-08 16:03:20 -0600

    confirmed table is not created automatically, which is good.

--- back/components/manageDB.js
+++ back/components/manageDB.js
@@ -4,6 +4,7 @@ function dbConnection() {
     const location = '../db/todo.db';
     let db = null;
     try {
+        //better-SQLite3 will create db file if not present
         db = new Database(location);
     }catch(e) {
         console.log(e);
@@ -11,4 +12,11 @@ function dbConnection() {
     }
     return db;
 }
+
+function newUser(db, {name, password}){
+    const addUserStmt = db.prepare('insert into users (name, password) values (?,?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)');
+    return addUserStmt.run([name, password]);
+}
+
 const db = dbConnection();
+console.log(newUser(db, {name: "test", password: "t3st"}));
