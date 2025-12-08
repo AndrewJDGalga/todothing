2abf25ed9455358f95078a9a059886aa69665ec8
AJDev <contact@andrewjonhardt.com>
2025-12-08 14:15:25 -0600

    prev DB existance check incompatible with Node.

---
+++ back/components/dbExist.js
@@ -0,0 +1,11 @@
+import { access, constants } from 'node:fs';
+import { DatabaseSync } from "node:sqlite";
+
+export default async function dbExist() {
+    const location = 'db/todo.db';
+    try {
+        const existCheck = await 
+    } catch (error) {
+        
+    }
+}
