bd002627cef175022c84bbe3d2a94125239ebf04
AJDev <contact@andrewjonhardt.com>
2025-12-08 14:29:01 -0600

    somehow missed the 'active dev' warning in node sql component, searching alts.

--- back/components/dbExist.js
+++ back/components/dbExist.js
@@ -1,11 +1,10 @@
 import { access, constants } from 'node:fs';
-import { DatabaseSync } from "node:sqlite";
+import { DatabaseSync } from 'node:sqlite';
 
 export default async function dbExist() {
     const location = 'db/todo.db';
-    try {
-        const existCheck = await 
-    } catch (error) {
-        
-    }
+    
+    const db = new DatabaseSync(location);
+
 }
+dbExist();
