95685295c20aa736c4168e8bfd8b37657762dae9
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2025-12-11 19:41:55 -0600

    confirmed uniqueness enforcement and error code associated.

--- back/components/manageDB.js
+++ back/components/manageDB.js
@@ -13,6 +13,7 @@ function dbConnection(location) {
     try {
         //better-SQLite3 will create db file if not present
         db = new Database(location, {verbose: console.log});
+        db.pragma('foreign_keys = on');
     }catch(e) {
         console.log(e);
         process.exit(1);
@@ -27,8 +28,22 @@ function runRawSQL(db, scriptFilePath) {
 
 //USER
 function addUser(db, {name, password}){
+    let res = '';
+    try{
+        const addUserStmt = db.prepare('insert into user (name, password, creation, modification) values (?,?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)');
+        res = addUserStmt.run(name, password);
+    }catch(e){
+        console.log(Object.keys(e));
+        console.log(e.code);
+        if(e.code === 'SQLITE_CONSTRAINT_UNIQUE') {
+            console.log('NOT UNIQUE!!!');
+        }
+    }
+    return res;
+    /*
     const addUserStmt = db.prepare('insert into user (name, password, creation, modification) values (?,?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)');
     return addUserStmt.run(name, password);
+    */
 }
 function removeUser(db, id) {
     const removeUserStmt = db.prepare('delete from user where id = ?');
@@ -59,17 +74,19 @@ function confirmUser(db, {providedName, providedPassword}){
 
 
 
-
+//TEST
 const db = dbConnection(location);
+/*
 console.log(runRawSQL(db, '../sqlScripts/step_list_table.sql'));
 console.log(runRawSQL(db, '../sqlScripts/user_table.sql'));
 console.log(runRawSQL(db, '../sqlScripts/task_list_table.sql'));
 console.log(runRawSQL(db, '../sqlScripts/user_task_list_table.sql'));
-
-
-//console.log(addUser(db, {name: 'test', password: 't35t'}));
+*/
+console.log(addUser(db, {name: 'test', password: 't35t'}));
+console.log(getUserByName(db, 'test'));
+//console.log(removeUser(db, 3));
+//console.log(getUserByName(db, 'test'));
 //console.log(getUserByName(db, 'test'));
 //console.log(getUserByID(db, 1));
-//console.log(removeUser(db, 1));
-//console.log(getUserByName(db, 'test'));
-//console.log(getUserByID(db, 1));
+
+db.close();
