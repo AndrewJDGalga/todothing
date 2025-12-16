a7cb4711d2796ffbae87b5a9e4773bd3fb7d2fc9
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2025-12-16 13:40:50 -0600

    init users_created_after + users_deleted_before.

--- back/sql/users_triggers.sql
+++ back/sql/users_triggers.sql
@@ -1,21 +1,27 @@
-create trigger users_created
+create trigger users_created_after
 after insert on users
 begin
-    insert into created
+    insert into created(iso_date)
+        values(strftime('%Y-%m-%d %H:%M:%f', 'now'));
+    insert into users_created(users_id, created_id)
+        values(new.id, last_insert_rowid());
 end; 
 
-create trigger users_modified
+create trigger users_modified_after
 after update on users
 for each row
 begin
     insert into modified(iso_date) 
         values(strftime('%Y-%m-%d %H:%M:%f', 'now'));
-
     insert into users_modified(users_id, modified_id) 
         values(new.id, last_insert_rowid());
 end;
 
-create trigger users_deleted
+create trigger users_deleted_before
 before delete on users
 begin
+    insert into deleted(iso_date, note)
+        values(strftime('%Y-%m-%d %H:%M:%f', 'now'), old.name);
+    insert into users_deleted(users_id, deleted_id)
+        values(old.id, last_insert_rowid());
 end;
