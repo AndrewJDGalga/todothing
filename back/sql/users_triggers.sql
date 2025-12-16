5d903a8eeeb94a457cf9971f5f3197d29cf8a64e
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2025-12-16 12:58:41 -0600

    revision of users_modified trigger, untested. Creation and deletion triggers, empty.

--- back/sql/users_triggers.sql
+++ back/sql/users_triggers.sql
@@ -1,8 +1,21 @@
-create trigger users_details_modified
-after update on users
-when old.name <> new.name OR old.password <> new.password
+create trigger users_created
+after insert on users
 begin
-    update users
-    set modification = strftime('%Y-%m-%d %H:%M:%f', 'now')
-    where id = old.id;
+
+end; 
+
+create trigger users_modified
+after update on users
+for each row
+begin
+    insert into modified(iso_date) 
+        values(strftime('%Y-%m-%d %H:%M:%f', 'now'));
+
+    insert into users_modified(users_id, modified_id) 
+        values(new.id, last_insert_rowid());
+end;
+
+create trigger users_deleted
+before delete on users
+begin
 end;
