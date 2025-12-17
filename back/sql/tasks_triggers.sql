ed439bb086b28168421dd8a58b1f9ae6756dbb62
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2025-12-17 15:47:12 -0600

    classic rookie OR vs AND mistake on tasks_bool_guard.

--- back/sql/tasks_triggers.sql
+++ back/sql/tasks_triggers.sql
@@ -2,9 +2,8 @@ create trigger tasks_bool_guard
 before insert on tasks
 for each row
 begin
-    select
-    case
-        when new.isComplete <> 0 OR new.isComplete <> 1 then
+    select case
+        when new.isComplete <> 0 AND new.isComplete <> 1 then
             raise (abort, 'isComplete must be fake bool: 1 OR 0.')
     end;
 end;
